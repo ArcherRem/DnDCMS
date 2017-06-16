5da0da62fd9f4ebac80f910db04c0a4f3791b807
Remco Merkx <rwam.merkx@hotmail.com>
2017-06-16 22:00:15 +0200

    Added GetAllSpells method to SpellContext

--- DnDCMSLibrary/Entities/Spell.cs
+++ DnDCMSLibrary/Entities/Spell.cs
@@ -1,36 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DnDCMSLibrary.Entities
+﻿namespace DnDCMSLibrary.Entities
 {
-    class Spell
+    [System.Serializable]
+    public class Spell
     {
         public int id;
-        public string name { get; private set; }
-        public string level { get; private set; }
-        public string type { get; private set; }
-        public bool ritual { get; private set; }
-        public string effect { get; private set; }
-        public string castingtime { get; private set; }
-        public string range { get; private set; }
-        public string components { get; private set; }
-        public string duration { get; private set; }
-
-        public Spell(int id, string name, string level, string type, bool ritual, string effect, string castingtime, string range, string components, string duration)
-        {
-            this.id = id;
-            this.name = name;
-            this.level = level;
-            this.type = type;
-            this.ritual = ritual;
-            this.effect = effect;
-            this.castingtime = castingtime;
-            this.range = range;
-            this.components = components;
-            this.duration = duration;
-        }
+        public string name { get; set; }
+        public string level { get; set; }
+        public string type { get; set; }
+        public bool ritual { get; set; }
+        public string effect { get; set; }
+        public string castingtime { get; set; }
+        public string range { get; set; }
+        public string components { get; set; }
+        public string duration { get; set; }
     }
 }
--- DnDCMSLibrary/Repositories/SpellContext.cs
+++ DnDCMSLibrary/Repositories/SpellContext.cs
@@ -3,10 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using DnDCMSLibrary.Entities;
+using DnDCMSLibrary.Interfaces;
 
 namespace DnDCMSLibrary.Repositories
 {
-    class SpellContext
+    public class SpellContext : ISpellRepository
     {
+        public List<Spell> GetAllSpells()
+        {
+            try
+            {
+                List<Spell> result = new List<Spell>();
+                using (SqlConnection sqlcon = Database.Connection)
+                {
+                    string query = "SELECT * FROM Spells";
+                    using (SqlCommand cmd = new SqlCommand(query, sqlcon))
+                    {
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                result.Add(CreateSpellFromReader(reader));
+                            }
+                        }
+                    }
+                    return result;
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+        private Spell CreateSpellFromReader(SqlDataReader reader)
+        {
+            Spell spell = new Spell()
+            {
+            id = Convert.ToInt32(reader["id"]),
+            name = reader["name"].ToString(),
+            level = reader["spelllevel"].ToString(),
+            type = reader["spelltype"].ToString(),
+            ritual = Convert.ToBoolean(reader["ritual"]),
+            effect = reader["effect"].ToString(),
+            castingtime = reader["casting time"].ToString(),
+            range = reader["range"].ToString(),
+            components = reader["components"].ToString(),
+            duration = reader["duration"].ToString(),
+        };
+            return spell;
+        }
     }
 }
+
