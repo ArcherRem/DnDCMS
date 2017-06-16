84c27a59705de2640e5663827b23b7444aa47b43
Remco Merkx <rwam.merkx@hotmail.com>
2017-06-16 18:19:03 +0200

    Added Folders and Enum class

---
+++ DnDCMSLibrary/Enums.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDCMSLibrary
+{
+    public enum Race
+    {
+
+    }
+
+    public enum SubRace
+    {
+
+    }
+
+
+}
