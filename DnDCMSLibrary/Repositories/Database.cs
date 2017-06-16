0c3383a2c6b322a792a886df346f557edf71f111
Remco Merkx <rwam.merkx@hotmail.com>
2017-06-16 21:37:13 +0200

    added database class and configuration reference

---
+++ DnDCMSLibrary/Repositories/Database.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace DnDCMSLibrary.Repositories
+{
+    class Database
+    {
+        private static readonly string connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+
+        public static SqlConnection Connection
+        {
+            get
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                return connection;
+            }
+        }
+    }
+}
